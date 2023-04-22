8e3c373296d16891d7c845e239e3e2981d462141
Sergey Lyulchenko <solo300m@yandex.ru>
2023-04-22 20:00:41 +0400

    ico commit

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,11 +1,17 @@
 package ru.netology.nmedia
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(getString(R.string.unboard))
+        Log.d("MainActivity", "onCreate: ${getString(R.string.unboard)}")
+        Log.d("MainActivity", "onCreate: ${resources.displayMetrics.heightPixels}")
+        Log.d("MainActivity", "onCreate: ${resources.displayMetrics.widthPixels}")
     }
 }
